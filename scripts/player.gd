e5db6515996b390d45b8e283b011f3f00283383b
TLT <spelujo@gmail.com>
2026-01-02 18:28:02 +0100

    Movimiento

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,22 +4,22 @@ extends CharacterBody3D
 @onready var head = $Head 
 @onready var mano = $Hand
 @onready var linterna = $Hand/SpotLight3D
+@onready var body_pivot = $Player
 
-
-@export var playerSpeed = 8.0
-@export var player_acc = 5.0
+@export var playerSpeed: float = 8.0
+@export var player_acc: float = 5.0
 @export var move_speed: float = 6.0
 @export var gravity: float = 24.0
 @export var jump_velocity: float = 6.0
 @export var camera_sens: float = 0.05
-@export var jumpForce = 8.0
-@export var camera_acc = 1.5
+@export var jumpForce: float = 8.0
+@export var camera_acc: float = 1.5
 
-
-var direction = Vector3.ZERO
+var direction: Vector3 = Vector3.ZERO
 var y_velocity: float = 6.0
-var head_y_axis = 0.0
-var camera_x_axis = 0.0
+var head_y_axis: float = 0.0
+var camera_x_axis: float = 0.0
+var spotlight_on: bool = true
 
 func _input(event):
 	if event is InputEventMouseMotion:
@@ -29,16 +29,24 @@ func _input(event):
 
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	
 
-func _physics_process(delta):
-	# Gravedad
-	direction = Input.get_axis("move_left", "move_right")* head.basis.x + Input.get_axis("move_forward", "move_back") * head.basis.z
+func _physics_process(delta: float) -> void:
+	# Entrada
+	var input_x = Input.get_axis("move_left", "move_right")
+	var input_z = Input.get_axis("move_forward", "move_back")
+	
+	# Dirección basaba en la rotación del cuerpo
+	direction = input_x * body_pivot.basis.x + input_z * body_pivot.basis.z
+	direction.y = 0.0
+	direction = direction.normalized()
+	
 	velocity = velocity.lerp(direction * playerSpeed + velocity.y * Vector3.UP, player_acc * delta)
 	
+	# Rotación cabeza y cámara
 	mano.rotation.y = -deg_to_rad(head_y_axis)
 	linterna.rotation.x = -deg_to_rad(camera_x_axis)
 	
+	body_pivot.rotation.y = -deg_to_rad(head_y_axis)
 	head.rotation.y = lerp(head.rotation.y, -deg_to_rad(head_y_axis), camera_acc * delta)
 	camara.rotation.x = lerp(camara.rotation.x, -deg_to_rad(camera_x_axis), camera_acc * delta)
 	
@@ -47,12 +55,19 @@ func _physics_process(delta):
 	else:
 		velocity.y -= gravity * delta
 	
+	# Encender apagar la linterna
+	if Input.is_action_just_pressed("spotlight_on_off") and spotlight_on:
+		spotlight_on = false
+		linterna.visible = false
+	elif Input.is_action_just_pressed("spotlight_on_off") and !spotlight_on:
+		spotlight_on = true
+		linterna.visible = true
+	
 	move_and_slide()
 	
 	
 """var is_starting_jump := Input.is_action_just_pressed("jump") and is_on_floor()
 	y_velocity -= gravity * delta
-	
 	
 	if is_starting_jump:
 		$Player/AnimationPlayer.play("Jump_Start")
@@ -102,6 +117,5 @@ func _physics_process(delta):
 		
 	# Aplicar velocidad vertical
 	velocity_3d.y = y_velocity
-	
 	
 	velocity = velocity_3d"""

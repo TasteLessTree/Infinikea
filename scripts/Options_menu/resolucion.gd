e9f09c9f098687f2a9c1bac0db73799338a9376c
TLT <spelujo@gmail.com>
2026-01-09 13:45:30 +0100

    FOV

--- scripts/Options_menu/resolucion.gd
+++ scripts/Options_menu/resolucion.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var option_button: OptionButton = $HBoxContainer/OptionButton
 
-const RESULUTION_DICTIONARY: Dictionary = {
+const RESOLUTION_DICTIONARY: Dictionary = {
 	"1152 x 648": Vector2i(1152, 648),
 	"1280 x 720": Vector2i(1280, 720),
 	"1920 x 1080": Vector2i(1920, 1080)
@@ -10,9 +10,11 @@ const RESULUTION_DICTIONARY: Dictionary = {
 
 func _ready() -> void:
 	option_button.item_selected.connect(on_resolution_selected)
+	add_resolution_items()
 
-func add_resolution() -> void:
-	pass
+func add_resolution_items() -> void:
+	for resolution_size_text in RESOLUTION_DICTIONARY:
+		option_button.add_item(resolution_size_text)
 	
 func on_resolution_selected(idx: int) -> void:
-	pass
+	DisplayServer.window_set_size(RESOLUTION_DICTIONARY.values()[idx])

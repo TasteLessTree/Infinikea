7c56cbf9ba9edddcf42340d8d3ced39565ecc57c
TLT <spelujo@gmail.com>
2026-01-26 14:15:08 +0100

    Intento de hacer IA path finding y buscar al jugador

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,13 +2,15 @@ extends CharacterBody3D
 
 @onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 @onready var animation_tree: AnimationTree = $AnimationTree
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
-@onready var armature: Node3D = $Armature
+@onready var ray_cast_3d: RayCast3D = $RayCast3D
 
 @export var player_path: NodePath
+@export_enum("Idle", "Neutral", "Searching", "Spotted", "Chasing", "Attack") var states: String
 
 var player = null
-var state_machine
+var state: String = "Idle"
 
 const SPEED: float = 7.5
 const SCREAM_RANGE: float = 4.0
@@ -16,28 +18,33 @@ const ATTACK_RANGE: float = 2.0
 
 func _ready() -> void:
 	player = get_node(player_path)
-	state_machine = animation_tree.get("parameters/playback")
 
 func _physics_process(_delta: float) -> void:
-	match state_machine.get_current_node():
-		"idle":
+	match state:
+		"Idle":
 			animation_tree.set("parameters/conditions/walking", true)
+			ray_scanning()
 
-		"walk":
+		"Neutral":
 			walk_or_run(0.0)
 
-		"run":
-			walk_or_run(0.5)
+		"Searching":
+			animation_player.play("scream")
+
+		_:
+			print("Unexpected token: " + state + ". Check spelling or add state")
+
+func ray_scanning() -> void:
+	if ray_cast_3d.is_colliding():
+		var collider = ray_cast_3d.get_collider()
 		
-		"scream":
-			look_towards(player.global_position)
-
-		"attack":
-			animation_tree.set("parameters/conditions/run", !target_in_range())
-			look_towards(player.global_position)
-
-		"hit":
-			pass
+		if collider == null:
+			return
+		
+		if collider == player:
+			print("Jugador detectado")
+			print("Cambiando estado")
+			state = "Neutral"
 
 func walk_or_run(running_speed: float) -> void:
 	velocity = Vector3.ZERO
@@ -45,11 +52,12 @@ func walk_or_run(running_speed: float) -> void:
 	var next_nav_point  = nav_agent.get_next_path_position()
 	velocity = (next_nav_point - global_position).normalized() * (SPEED + running_speed)
 
-	# Atacar si el jugador está cerca
+	ray_scanning()
+
+	# Gritar si el jugador está cerca y atacarle si está a un más cerca
 	animation_tree.set("parameters/conditions/scream", scream_in_range())
 	animation_tree.set("parameters/conditions/attack", target_in_range())
 
-	# Mientras está andando
 	look_towards(next_nav_point)
 
 	move_and_slide()
@@ -62,4 +70,4 @@ func scream_in_range() -> bool:
 
 func look_towards(target: Vector3) -> void:
 	var look_pos = Vector3(target.x, global_position.y, target.z)
-	armature.look_at(look_pos, Vector3.UP)
+	look_at(look_pos, Vector3.UP)

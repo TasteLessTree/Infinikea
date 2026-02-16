1cebd795c2d6ec92ea670b1e2cd913da30b60d62
Filipe2721 <filipe2721@gmail.com>
2026-02-16 20:52:20 +0100

    Ventana e intento de dia y noche

--- scripts/world_environment.gd
+++ scripts/world_environment.gd
@@ -1,6 +1,12 @@
 extends WorldEnvironment
 
 @onready var ambient_lightbulb: AudioStreamPlayer = $ambient_lightbulb
+@onready var directional_light_3d: DirectionalLight3D = $"../DirectionalLight3D"
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
 
 func _ready() -> void:
 	ambient_lightbulb.play()
+	_set_sun()
+	
+func _set_sun():
+	animation_player.play("ciclo_dia_noche")

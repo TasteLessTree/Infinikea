cde76b3132b61a819258f0cb963256ba8a211555
Filipe2721 <filipe2721@gmail.com>
2026-02-17 11:51:43 +0100

    Ikea imports

--- scripts/enviroment/world_environment.gd
+++ scripts/enviroment/world_environment.gd
@@ -2,10 +2,16 @@ extends WorldEnvironment
 
 @onready var ambient_lightbulb: AudioStreamPlayer = $ambient_lightbulb
 @onready var env: Environment = environment
+@onready var directional_light_3d: DirectionalLight3D = $"../DirectionalLight3D"
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
 
 func _ready() -> void:
 	ambient_lightbulb.play()
 	CicloDiaNoche.cambio_estado.connect(_on_ciclo_cambiado)
+	_set_sun()
+	
+func _set_sun():
+	animation_player.play("ciclo_dia_noche")
 
 # Ciclo de día y noche
 func _on_ciclo_cambiado(noche: bool) -> void:
--- scripts/player/mano.gd
+++ scripts/player/mano.gd
@@ -1,28 +1,24 @@
 extends Node3D
-
+ 
 @onready var linterna = $SpotLight3D
 @onready var sfx_flashlight = $"../../../Sonidos/sfx_flashlight"
-
-
-
+ 
 var spotlight_on: bool = false
 var current_item_instance: Node3D = null
-
+ 
 func _ready():
 	Inventario.slot_selected.connect(_update_held_item)
-
+ 
 func clear_item():
 	if current_item_instance:
 		current_item_instance.queue_free()
 		current_item_instance = null
-
+ 
 func show_item(item_data: ItemData):
 	clear_item()
-	
 	if item_data == null:
 		_desactivar_linterna_visual()
 		return
-	
 	if item_data.mesh_scene:
 		current_item_instance = item_data.mesh_scene.instantiate()
 		if current_item_instance is RigidBody3D:
@@ -32,38 +28,31 @@ func show_item(item_data: ItemData):
 		current_item_instance.position = Vector3.ZERO
 		current_item_instance.rotation = Vector3.ZERO
 		add_child(current_item_instance)
-		
-		add_child(current_item_instance) 
-		
 		# --- LÓGICA PARA EL MATERIAL OVERLAY ---
 		# Buscamos el nodo 'brillo' que es un MeshInstance3D
 		var mesh_node = current_item_instance.get_node_or_null("flashlight_low") as MeshInstance3D
-		
 		if mesh_node:
 			# Si quieres quitarlo completamente al estar en la mano:
 			mesh_node.material_overlay = null 
-			
 			# O si quieres que dependa de si la linterna está encendida:
 			# _actualizar_brillo_overlay(mesh_node)
-		
 	if (item_data.item_name == "Linterna"):
 		# No alteramos 'spotlight_on' aquí para que mantenga su estado al cambiar de slot
 		linterna.light_energy = 2 if spotlight_on else 0
-
+ 
 		
 	else:
 		_desactivar_linterna_visual()
-		
 
+ 
 func _update_held_item(slot_index: int):
 	var item = Inventario.hotbar[slot_index]
 	show_item(item)
-		
 
+ 
 func _input(_event: InputEvent):
 	# 1. Obtener qué item tenemos en la mano actualmente
 	var item_actual = Inventario.hotbar[Inventario.selected_slot] 
-	
 	# 2. Solo permitir el input si el item actual es la linterna
 	if item_actual != null and item_actual.item_name == "Linterna":
 		if Input.is_action_just_pressed("spotlight_on_off"):
@@ -71,17 +60,15 @@ func _input(_event: InputEvent):
 			sfx_flashlight.play() 
 			spotlight_on = !spotlight_on
 			linterna.light_energy = 2 if spotlight_on else 0
-		
-		
+
 func _desactivar_linterna_visual():
 	linterna.light_energy = 0
-	
 func _remover_colisiones(nodo: Node):
 	for child in nodo.get_children():
 		if child is CollisionShape3D or child is CollisionPolygon3D or child is StaticBody3D or child is RigidBody3D:
 			child.queue_free() # Elimina la colisión solo de la instancia en la mano
 		_remover_colisiones(child) # Recursivo para sub-nodos
-
+ 
 func _actualizar_brillo_overlay(mesh_node: MeshInstance3D):
 	if mesh_node:
 		# Si la linterna está ON, mantenemos el overlay (o lo reasignamos)
